64c5955c6fb9e74839124397728e85edff5b028e
Waheed Khan <wahid.khan74@gmail.com>
2021-09-13 14:15:17 +0000

    first technical question

---
+++ src/com/simplilearn/pyramidpattern/PyramidPattern.java
@@ -0,0 +1,42 @@
+package com.simplilearn.pyramidpattern;
+
+import java.util.Scanner;
+
+public class PyramidPattern {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the row to print :");
+		int rows = input.nextInt();
+		
+		System.out.println("Output Pattern :");
+		printPattern(rows);
+	}
+	
+	private static void printPattern(int rows) {
+		// for loop for the rows
+		for (int i = 1; i <= rows ; i++) {
+			
+			// white spaces in the front of numbers
+			int numberOfWhiteSpaces = rows-i;
+			
+			// print leading white spaces
+			printData(" ",numberOfWhiteSpaces);
+			
+			//print numbers
+			printData("A" +" ",i);
+			
+			//move to next line
+			System.out.println("");
+			
+		}
+	}
+
+	//utility function to print string given times
+	private static void printData(String str, int times) {
+		for (int j = 0; j < times; j++) {
+			System.out.print(str);
+		}
+	}
+}
